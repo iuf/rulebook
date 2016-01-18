1efa234a182c65cf8ec06d790f8f7923a5cf0076
Felix Dietze <felix@beerleader.net>
2016-01-18 19:13:20 +0100

    simplify checkout

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,15 +4,10 @@
 git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
 git fetch
 
-current_branch=$(git rev-parse --abbrev-ref HEAD)
-
 # track all remote branches, to make them accessible to latexdiff-vc
 for remote in `git branch -r | grep -v /HEAD`; do
     git checkout --track $remote
 done
-
-git checkout $current_branch
-git branch
 
 
 
