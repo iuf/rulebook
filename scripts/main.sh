caa2507d377869cf719ae3d354d76d5fa78488f5
Felix Dietze <felix@beerleader.net>
2016-01-26 14:27:09 +0100

    cleanup main.sh

--- scripts/main.sh
+++ scripts/main.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-echo $PATH
-which rcs-latexdiff
-
-# create iuf-rulebook.pdf
 make rulebook
-
-#TODO: translations
-
-
-#create diffs (TODO)
-touch diff-branches
 make diff
-# - for oldcommit in $(cat diff-branches); do make diff NEWCOMMIT=$TRAVIS_BRANCH OLDCOMMIT=$oldcommit; done
+#TODO: translations
