9b5c6d07d7ba72bf293de187d71f9d3c190629e6
Felix Dietze <felix@beerleader.net>
2016-02-22 20:35:35 +0100

    fix diff

--- scripts/build/diff.sh
+++ scripts/build/diff.sh
@@ -12,6 +12,6 @@ mkdir -p tmp
 
 cp --archive --no-target-directory src tmp/src_diff-$DIFFBRANCH
 latexdiff-vc --git --flatten --force --exclude-textcmd="part,chapter,section,subsection,subsubsection" --revision=$DIFFBRANCH src/iuf-rulebook.tex
-mv src/iuf-rulebook-diff-$DIFFBRANCH.tex src/iuf-rulebook-$BRANCH-diff-$DIFFBRANCH.tex
+mv src/iuf-rulebook-diff$DIFFBRANCH.tex src/iuf-rulebook-$BRANCH-diff-$DIFFBRANCH.tex
 
 scripts/build/pdf.sh tmp/src_diff-$DIFFBRANCH $BRANCH-diff-$DIFFBRANCH
