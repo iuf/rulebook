6a6b2859deaefe07637b563a87e86055048c87ed
Felix Dietze <felix@beerleader.net>
2016-02-22 20:48:29 +0100

    create dir

--- scripts/build/pdf.sh
+++ scripts/build/pdf.sh
@@ -1,13 +1,14 @@
 #!/bin/bash -e
 
-mkdir -p tmp/out
-mkdir -p pdf
-
 SRC=$1
 FILE_SUFFIX=$2
 # turn src/ -> tmp/out
 # turn tmp/src_de/ -> tmp/out_de
 OUT=$(echo tmp/$SRC | sed s/tmp\\/tmp\\//tmp\\// | sed s/src/out/)
+
+mkdir -p $OUT
+mkdir -p pdf
+
 LATEXARGS=" -output-directory=$OUT -interaction=batchmode -file-line-error -halt-on-error"
 
 TEXINPUTS=$SRC: pdflatex $LATEXARGS -draftmode $SRC/iuf-rulebook.tex
