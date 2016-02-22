9052907d56db19a21d5e2706a9980a5e5777b6ae
gossi <github@gos.si>
2016-02-22 17:38:45 +0100

    script for generating pdf

---
+++ scripts/build/pdf.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+
+mkdir -p tmp/out
+mkdir -p pdf
+
+SRC=$1
+FILE_SUFFIX=$2
+# turn src/ -> tmp/out
+# turn tmp/src_de/ -> tmp/out_de
+OUT=$(echo tmp/$SRC | sed s/tmp\\/tmp\\//tmp\\// | sed s/src/out/)
+LATEXARGS=" -output-directory=$OUT -interaction=batchmode -file-line-error -halt-on-error"
+
+TEXINPUTS=$SRC: pdflatex $LATEXARGS -draftmode $SRC/iuf-rulebook.tex
+TEXINPUTS=$SRC: pdflatex $LATEXARGS            $SRC/iuf-rulebook.tex
+mv $OUT/iuf-rulebook.pdf pdf/iuf-rulebook-$FILE_SUFFIX.pdf;
