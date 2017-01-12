28048559302167aca5daddada5115b715ecd1801
Scott Wilton <scott.o.wilton@gmail.com>
2017-01-11 21:39:56 -0700

    turn on diff build and use correct latexdiff (stand alone) to fix travis dependecies

--- scripts/build/diff.sh
+++ scripts/build/diff.sh
@@ -11,7 +11,7 @@ fi
 mkdir -p tmp
 
 cp --archive --no-target-directory src tmp/src_diff-$DIFFBRANCH
-latexdiff-vc --git --flatten --force --exclude-textcmd="part,chapter,section,subsection,subsubsection" --revision=$DIFFBRANCH src/iuf-rulebook.tex
+latexdiff-vc --git --so --flatten --force --exclude-textcmd="part,chapter,section,subsection,subsubsection" --revision=$DIFFBRANCH src/iuf-rulebook.tex
 mv src/iuf-rulebook-diff$DIFFBRANCH.tex src/iuf-rulebook-$BRANCH-diff-$DIFFBRANCH.tex
 
 scripts/build/pdf.sh tmp/src_diff-$DIFFBRANCH $BRANCH-diff-$DIFFBRANCH
