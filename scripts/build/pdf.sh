6e5861f74b0fd4f51db8cb4c87ef83cf18212e67
Scott Wilton <scott.o.wilton@gmail.com>
2017-01-25 22:58:57 -0600

    Clean up build pdf.sh

--- scripts/build/pdf.sh
+++ scripts/build/pdf.sh
@@ -12,6 +12,7 @@ mkdir -p pdf
 
 LATEXARGS=" -output-directory=$OUT -file-line-error -halt-on-error"
 
-TEXINPUTS=$SRC: openout_any=a latexmk -pdf -quiet $LATEXARGS $SRC/iuf-rulebook.tex #latemk 4.24 available
+TEXINPUTS=$SRC: openout_any=a latexmk -pdf -quiet $LATEXARGS $SRC/iuf-rulebook.tex
 # remove -quiet if the build is failing to figure out where
+
 mv $OUT/iuf-rulebook.pdf pdf/iuf-rulebook-$FILE_SUFFIX.pdf;
