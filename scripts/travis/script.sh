e72ec2e3f6abd76cee780f50d429d9525edc2ec9
Scott Wilton <scott.o.wilton@gmail.com>
2017-01-25 22:58:41 -0600

    Folding log for Travis build

--- scripts/travis/script.sh
+++ scripts/travis/script.sh
@@ -2,5 +2,12 @@
 set -e # POSIX version of bash -e
 
 make rulebook
-scripts/build/diff-all.sh
+echo # insert blank line in travis output
+#echo -en 'travis_fold:start:build_diffs\\r'
+#scripts/build/diff-all.sh
+#echo -en 'travis_fold:end:build_diffs\\r'
+echo  #insert blank line in travis output
+echo -en 'travis_fold:start:build_translations\\r'
+echo starting diff
 scripts/build/translation.sh
+echo -en 'travis_fold:end:build_translations\\r'
