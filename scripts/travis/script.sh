6238b3830b7a6bc8ed45fde6ebc3603b72477a9a
Scott Wilton <scott.o.wilton@gmail.com>
2017-01-31 12:15:32 -0600

    Turn diff build back on

--- scripts/travis/script.sh
+++ scripts/travis/script.sh
@@ -3,9 +3,9 @@ set -e # POSIX version of bash -e
 
 make rulebook
 echo # insert blank line in travis output
-#echo -en 'travis_fold:start:build_diffs\\r'
-#scripts/build/diff-all.sh
-#echo -en 'travis_fold:end:build_diffs\\r'
+echo -en 'travis_fold:start:build_diffs\\r'
+scripts/build/diff-all.sh
+echo -en 'travis_fold:end:build_diffs\\r'
 echo  #insert blank line in travis output
 echo -en 'travis_fold:start:build_translations\\r'
 echo starting diff
