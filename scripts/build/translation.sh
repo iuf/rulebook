a9efc2bf24a2560b4976d1062ba498b43a1238e0
Scott Wilton <scott.o.wilton@gmail.com>
2017-02-08 01:13:52 -0600

    Make translation script even quieter

--- scripts/build/translation.sh
+++ scripts/build/translation.sh
@@ -49,13 +49,24 @@ while getopts :hvcd opt; do
 done
 shift "$((OPTIND-1))" # Shift off the options and optional --.
 
+verbose_cmd() {
+    # append 'verbose_cmd' before any command and it's output is silenced unless verbose is true
+    if [[ $VERBOSE -eq 1 ]]; then
+        "$@" > /dev/null
+    else
+        "$@"
+    fi
+}
+
 if [[ $DEBUG -eq 0 ]]; then
     rm -rf tmp # clean up tmp dir incase the problem is there
     CHAPTERS="01_general.tex" # only translate the first chater
 fi
 
 rm -rf .tx
-tx init --host=https://www.transifex.com
+echo "Configuring Transifex..."
+verbose_cmd tx init --host=https://www.transifex.com
+echo "Done."
 
 mkdir -p tmp
 mkdir -p pdf
@@ -76,45 +87,52 @@ rm -f $CHAPTERDIR/*.bak # because of this, we have to delete the .bak files afte
 
 # extract translation templates for each chapter and write them to transifex config file (.tx/config)
 # (for current branch)
+echo "Extracting Translation Templates..."
 for CHAPTER in $CHAPTERS; do
     SLUG=$(echo $CHAPTER | sed -e "s/[0-9][0-9]_\(.*\)\.tex/\1/")
-    echo $SLUG
-    echo $CHAPTERDIR/$CHAPTER
+    verbose_cmd echo $SLUG
+    verbose_cmd echo $CHAPTERDIR/$CHAPTER
     echo -e '0r config/po4a-escape.tex\nw' | ed -s $CHAPTERDIR/$CHAPTER # add po4a escape commands to the beginning of each chapter
-    TEXINPUTS=$CHAPTERDIR po4a-gettextize --format latex --master $CHAPTERDIR/$CHAPTER --po tmp/po/${SLUG}/template.pot $PO4ACHARSETS
+    TEXINPUTS=$CHAPTERDIR verbose_cmd po4a-gettextize --format latex --master $CHAPTERDIR/$CHAPTER --po tmp/po/${SLUG}/template.pot $PO4ACHARSETS
 
     sed -i.bak 's~charset=CHARSET~charset=UTF-8~' tmp/po/${SLUG}/template.pot # fix charset because po4a isn't setting it correctly
 
-    tx set --auto-local --resource=rulebook-$BRANCH.$SLUG "tmp/po/${SLUG}/<lang>.po" --type PO --source-lang en --source-file tmp/po/${SLUG}/template.pot --execute
+    verbose_cmd tx set --auto-local --resource=rulebook-$BRANCH.$SLUG "tmp/po/${SLUG}/<lang>.po" --type PO --source-lang en --source-file tmp/po/${SLUG}/template.pot --execute
 done
+echo "Done."
 
 # upload all tempates to transifex
-tx push --source
+echo "Pushing to transifex..."
+verbose_cmd tx push --source
+echo "Done."
 # download all translated strings from transifex
-tx pull --all
+echo "Pulling from transifex..."
+verbose_cmd tx pull --all
+echo "Done."
 
 # extracts the list of languages by looking at one chapter's po-files
 LANGUAGES=$(ls tmp/po/$(ls tmp/po | head -1)/*.po | xargs -n1 basename | sed "s/\\.po$//")
 
-echo "Languages:"
-echo $LANGUAGES
+verbose_cmd echo "Languages:"
+verbose_cmd echo $LANGUAGES
 
 for LANG in $LANGUAGES; do
     mkdir -p tmp/src_$LANG
     rsync -a tmp/src_translation/ tmp/src_$LANG
 done
 
-ls tmp
-ls tmp/src_translation/chapters
+verbose_cmd ls tmp
+verbose_cmd ls tmp/src_translation/chapters
 
 for CHAPTER in $CHAPTERS; do
     SLUG=$(echo $CHAPTER | sed -e "s/[0-9][0-9]_\(.*\)\.tex/\1/")
 
     TEXINPUTS=./tmp/src_translation/chapters po4a --variable chapter_file=$CHAPTER --variable chapter_slug=$SLUG $PO4ACHARSETS config/po4a.cfg
-done
+done > /dev/null
 
 #TODO: language specific titlepage
 
 for LANG in $LANGUAGES; do
+    echo "Building pdf for $LANG"
     scripts/build/pdf.sh $VERBOSE_FLAG $CLEAN_FLAG -s tmp/src_$LANG -o iuf-rulebook-$BRANCH-$LANG.pdf iuf-rulebook.tex
 done
