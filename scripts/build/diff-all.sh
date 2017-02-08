e113533da2153696c408c1ad6195896f1bb3355b
Scott Wilton <scott.o.wilton@gmail.com>
2017-02-08 00:51:01 -0600

    Update diff scripts to use pdf build script and flags

--- scripts/build/diff-all.sh
+++ scripts/build/diff-all.sh
@@ -1,6 +1,61 @@
 #!/usr/bin/env bash
 set -e # POSIX version of bash -e
 
-for DIFFBRANCH in $(cat diff-branches); do
-    scripts/build/diff.sh $DIFFBRANCH
+show_help() {
+cat << EOF
+Usage: ${0##*/} [-hvcd]
+Build the IUF Rulebook difference pdf comparing the current branch all branches in diff-branches.
+
+    -h     display this help and exit
+    -v     verbose (do not run latexmk quietly)
+    -c     clean mode: clean up all latex temp files before building pdf
+    -d     debug mode: only diff the first chapter and only diff against the first diff-branch, because its faster
+EOF
+}
+
+# Defaults variables:
+
+VERBOSE_FLAG=""
+VERBOSE=1
+CLEAN_FLAG=""
+DEBUG_FLAG=""
+DEBUG=1
+
+
+OPTIND=1 # Safe code
+while getopts :hvcd opt; do
+  case $opt in
+    h)
+        show_help
+        exit 0
+        ;;
+    v)  VERBOSE_FLAG="-v"
+        VERBOSE=0
+        ;;
+    c)  CLEAN_FLAG="-c" 
+        ;;
+    d)  DEBUG_FLAG="-d"
+        DEBUG=0
+        ;;
+    \?)
+        show_help >&2
+        exit 1
+        ;;
+  esac
+done
+shift "$((OPTIND-1))" # Shift off the options and optional --.
+
+if [[ $DEBUG -eq 0 ]]; then
+  DIFFBRANCHES=$(head -1 diff-branches)
+else
+  DIFFBRANCHES=$(cat diff-branches)
+fi
+
+if [[ $VERBOSE -eq 0 ]]; then
+  echo "Diffing on branches:"
+  echo $DIFFBRANCHES
+fi
+
+for DIFFBRANCH in $DIFFBRANCHES; do
+    scripts/build/diff.sh $VERBOSE_FLAG $CLEAN_FLAG $DEBUG_FLAG $DIFFBRANCH
 done
--- scripts/build/diff.sh
+++ scripts/build/diff.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 set -e # POSIX version of bash -e
 
-DIFFBRANCH=$1
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 SRC=src
 CHAPTERDIR=src/chapters
 SHORTCHAPTERDIR=${CHAPTERDIR##src/} # remove src/ from the start of CHAPTERDIR
 CHAPTERS=$(ls $CHAPTERDIR | grep ".*\.tex$")
 
-LATEXARGS="-file-line-error -halt-on-error"
 EXCLUDE_TEXTCMDS="part,chapter,section,subsection,subsubsection,iftoggle,comment2016" # for latexdiff
 
 function clean_up {
@@ -24,6 +22,71 @@ function clean_up {
 
 trap clean_up INT TERM SIGHUP SIGINT SIGTERM
 
+show_help() {
+cat << EOF
+Usage: ${0##*/} [-hvcd] DIFFBRANCH
+Build the IUF Rulebook difference pdf comparing the current branch with DIFFBRANCH.
+
+    -h      display this help and exit
+    -v      verbose (do not run latexmk quietly)
+    -c      clean mode: clean up all latex temp files before building pdf
+    -d      debug mode: only diff the first chapter, because its faster
+EOF
+}
+
+# Defaults variables:
+
+VERBOSE_FLAG=""
+VERBOSE=1
+CLEAN_FLAG=""
+DEBUG_FLAG=""
+DEBUG=1
+
+
+OPTIND=1 # Safe code
+while getopts :hvcd opt; do
+  case $opt in
+    h)
+        show_help
+        exit 0
+        ;;
+    v)  VERBOSE_FLAG="-v"
+        VERBOSE=0
+        ;;
+    c)  CLEAN_FLAG="-c" 
+        ;;
+    d)  DEBUG_FLAG="-d"
+        DEBUG=0
+        ;;
+    \?)
+        show_help >&2
+        exit 1
+        ;;
+  esac
+done
+shift "$((OPTIND-1))" # Shift off the options and optional --.
+
+verbose_cmd() {
+    # append 'verbose_cmd' before any command and it's output is silenced unless verbose is true
+    if [[ $VERBOSE -eq 1 ]]; then
+        "$@" > /dev/null
+    else
+        "$@"
+    fi
+}
+
+DIFFBRANCH=$1
+
+if [[ $DEBUG -eq 0 ]]; then
+  rm -rf tmp # clean up tmp dir incase the problem is there
+  CHAPTERS="01_general.tex" # only translate the first chater
+fi
+
+# if [[ $VERBOSE -eq 0 ]]; then
+#   echo "Diffing on branches:"
+#   echo $DIFFBRANCHES
+# fi
+
 if [[ ! $DIFFBRANCH ]]; then
     echo "usage: $0 DIFFBRANCH"
     exit 1
@@ -38,7 +101,7 @@ mkdir -p tmp/out_diff_$DIFFBRANCH
 #TODO: cp
 rsync -a  src/ tmp/src_original # copy original source before starting changes
 
-echo "Starting find and replace changes for diff..."
+verbose_cmd echo "Starting find and replace changes for diff..."
 
 # replace iftoggles that have a true and a false option with only the true option
 sed -i.bak 's~^[[:blank:]]*\\iftoggle{[[:alnum:]_][[:alnum:]_]*}{\\input{\([[:alnum:]_\/][[:alnum:]_\/]*\)}}{\\input{\([[:alnum:]_\/][[:alnum:]_\/]*\)}}~\\input{\1}~' $CHAPTERDIR/*.tex
@@ -50,34 +113,33 @@ sed -i.bak 's~^[[:blank:]]*\\iftoggle{[[:alnum:]_][[:alnum:]_]*}{\\input{\([[:al
 # the -i.bak is required so SED works on both OSX and Linux (BSD and GNU sed)
 rm -f $CHAPTERDIR/*.bak # because of this, we have to delete the .bak files after
 
-echo "Done"
+verbose_cmd echo "Done"
+
 
 # create tex diff files for each chapter:
 for CHAPTER in $CHAPTERS; do
-    echo $CHAPTER
+    verbose_cmd echo "Diffing on chapter file: $CHAPTER"
     SLUG=$(echo $CHAPTER | cut -f 1 -d '.')
-    latexdiff-vc --git --so --flatten  --force -r $DIFFBRANCH $CHAPTERDIR/$CHAPTER
+    verbose_cmd latexdiff-vc --git --so --flatten  --force -r $DIFFBRANCH $CHAPTERDIR/$CHAPTER
     # --exclude-textcmd=$EXCLUDE_TEXTCMDS
-    mv -v $CHAPTERDIR/$SLUG-diff$DIFFBRANCH.tex tmp/src_diff_$DIFFBRANCH/$SHORTCHAPTERDIR/$SLUG.tex
+    verbose_cmd mv -v $CHAPTERDIR/$SLUG-diff$DIFFBRANCH.tex tmp/src_diff_$DIFFBRANCH/$SHORTCHAPTERDIR/$SLUG.tex
 done
-# remember to do something about toggle include for std skills
+# TODO: remember to do something about toggle include for std skills
 
 # create title-page diff:
-latexdiff-vc --git --so --force -r $DIFFBRANCH -r $BRANCH $SRC/titlepage.tex
-mv -v $SRC/titlepage-diff$DIFFBRANCH-$BRANCH.tex tmp/src_diff_$DIFFBRANCH/titlepage.tex # move titlepage diff to tmp
+verbose_cmd latexdiff-vc --git --so --force -r $DIFFBRANCH -r $BRANCH $SRC/titlepage.tex
+verbose_cmd mv -v $SRC/titlepage-diff$DIFFBRANCH-$BRANCH.tex tmp/src_diff_$DIFFBRANCH/titlepage.tex # move titlepage diff to tmp
 rm -rf $SRC/titlepage-old* # remove tmp files created with latexdiff
 
 # copy all source files to tmp except chapters (because they're already there):
-rsync -az --ignore-existing --exclude $SHORTCHAPTERDIR src/ tmp/src_diff_$DIFFBRANCH/
+rsync -az --ignore-existing src/ tmp/src_diff_$DIFFBRANCH/ #***
 # --ignore-existing because the diff'ed titlepage is already in the tmp diff directory
 
 # add latexdiff preamble code to existing preamble:
 cat dependencies/latexdiff-preamble.tex >> tmp/src_diff_"$DIFFBRANCH"/preamble.tex
 
 # compile the actual pdf and put it in the pdf dir:
-TEXINPUTS=tmp/src_diff_$DIFFBRANCH: latexmk -pdf -quiet $LATEXARGS -output-directory=tmp/out_diff_$DIFFBRANCH tmp/src_diff_$DIFFBRANCH/iuf-rulebook.tex
-# remove -quiet if the build is failing to figure out where
-mv tmp/out_diff_$DIFFBRANCH/iuf-rulebook.pdf pdf/iuf-rulebook-$BRANCH-diff-$DIFFBRANCH.pdf
+scripts/build/pdf.sh $VERBOSE_FLAG $CLEAN_FLAG -s tmp/src_diff_$DIFFBRANCH -o iuf-rulebook-$BRANCH-diff-$DIFFBRANCH.pdf iuf-rulebook.tex
 
 clean_up
 
