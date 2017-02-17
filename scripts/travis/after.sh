2890f03a060769771132591bc56e4e824116c939
Scott Wilton <scott.o.wilton@gmail.com>
2017-02-16 23:43:52 -0600

    Travis fix try copy to upload directory

--- scripts/travis/after.sh
+++ scripts/travis/after.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 set -e # POSIX version of bash -e
 
-try_cp () { cp $1 $2 2>/dev/null || : ; } # try to copy files, but ignore errors if it doesn't work
-ls -l pdf
-
 mkdir -p upload
 
-try_cp pdf/*.pdf upload
+if ls pdf/*.pdf >/dev/null 2>&1; then cp pdf/*.pdf upload; fi # only copy files if they exist
 
 # Upload the log files too, and depending on how far the script gets, there are two possible locations:
-try_cp pdf/*.log upload
-try_cp tmp/latexmk/*.log upload
+if ls pdf/*.log >/dev/null 2>&1; then cp pdf/*.log upload; fi # only copy files if they exist
+if ls tmp/latexmk/*.log >/dev/null 2>&1; then cp tmp/latexmk/*.log upload; fi # only copy files if they exist
 
+echo "Files:"
+ls -1 upload
+echo
+echo "Uploading files..."
 ncftpput -u $FTPUSER -p $FTPPASS -R unicycling.org /temp upload/
+echo "Done."
